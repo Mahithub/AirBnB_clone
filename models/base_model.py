d10d09cd15a67e16311667e38872130fffb7ac4e
Mahithub <recappingmovie@gmail.com>
2024-05-14 06:18:12 -0700

    commited

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 
+import models
 from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
+        if len(kwargs != 0):
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
+
+
 
     def __str__(self):
         print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)):
@@ -15,7 +28,7 @@ class BaseModel:
         self.updated_at = datetime.now()
     def to_dict(self):
         redict = self.__dict__.copy()
-        redict['created_at'] = self.created_at.isoformat()
-        redict['updated_at'] = self.updated_at.isoformat()
-        redict['__class__'] = self.__class__.__name__
+        redict["created_at"] = self.created_at.isoformat()
+        redict["updated_at"] = self.updated_at.isoformat()
+        redict["__class__"] = self.__class__.__name__
         return redict
